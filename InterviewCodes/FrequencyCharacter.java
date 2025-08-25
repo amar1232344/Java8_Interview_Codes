ba588ed963e43623fb2f86a56b6b59cb3e3ae7f9
amar1232344 <65810430+amar1232344@users.noreply.github.com>
2025-08-25 21:20:13 +0530

    Count the frequency of character in string using Java 8 Streams.

--- InterviewCodes/FrequencyCharacter.java
+++ InterviewCodes/FrequencyCharacter.java
@@ -8,7 +8,9 @@ import java.util.stream.Collectors;
 public class FrequencyCharacter {
 public static void main(String[] args) {
 String input="acbcd";
-Map<String,Long> frequencyChar=Arrays.stream(input.split("")).collect(Collectors.groupingBy(Function.identity(),Collectors.counting()));
+Map<String,Long> frequencyChar=Arrays.stream(input.split("")).collect(Collectors.groupingBy(
+Function.identity(),
+Collectors.counting()));
 System.out.println(frequencyChar);
 }
 }
